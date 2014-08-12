cd749c6a2cf14c0d567f6da8f5768fee49fbf559
Mark Piper <mark.piper@colorado.edu>
2014-08-12 06:36:41 -0600

    Remove helper scripts from model directory
    They’re superceded by methods in **build_model_rpm.py**, and no longer
    needed.

--- hydrotrend/get_source.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Gets the source code for a model.
-# Mark Piper (mark.piper@colorado.edu)
-
-target=$HOME/rpmbuild/SOURCES
-svn export --quiet \
-    https://csdms.colorado.edu/svn/hydrotrend/trunk $target/trunk
-test -d $target/trunk
-exit $?
