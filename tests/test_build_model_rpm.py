f19a982d25e44762298fd6def44b9566b4e0d6e0
Mark Piper <mark.piper@colorado.edu>
2014-08-19 11:23:12 -0600

    Include nose tests for hydrotrend

---
+++ tests/test_build_model_rpm.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+from build_model_rpm import BuildModelRPM
+from nose.tools import *
+
+@raises(TypeError)
+def test_fail_with_no_parameters():
+    BuildModelRPM(None)
+
+@raises(TypeError)
+def test_fail_with_one_parameter():
+    BuildModelRPM("hydrotrend")
+
+def test_hydrotrend_version_none():
+    BuildModelRPM("hydrotrend", None)
+
+def test_hydrotrend_version_head():
+    BuildModelRPM("hydrotrend", "head")
+
+#def test_hydrotrend_tagged_version():
+#    BuildModelRPM("hydrotrend", "3.0.2")
